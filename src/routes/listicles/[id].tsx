2aaeb1fcdc89e5cd80152a639de076045b4fc022
Jacob Goodwin <jacob.goodwin@gmail.com>
2022-12-29 17:45:43 -0800

    get pre-event stream stuff working

--- src/routes/listicles/[id].tsx
+++ src/routes/listicles/[id].tsx
@@ -1,4 +1,4 @@
-import { For, Suspense } from 'solid-js';
+import { createResource, createSignal, For, Show, Suspense } from 'solid-js';
 import { A, RouteDataArgs, Title, useRouteData } from 'solid-start';
 import { createServerData$ } from 'solid-start/server';
 
@@ -6,10 +6,14 @@ import Loader from '~/components/loader';
 import { Listicle } from '~/data-types';
 
 export function routeData({ params }: RouteDataArgs) {
+  // This is double fetching, once server-side, and again client side
+  // when `key` is passed a function.
+  // see bug: https://github.com/solidjs/solid-start/issues/453
   return createServerData$(
-    async (key) => {
+    async (id) => {
+      console.log('refetching on server');
       const response = await fetch(
-        `http://localhost:3001/listicles/${key}?_embed=entries`
+        `http://localhost:3001/listicles/${id}?_embed=entries`
       );
       const data = (await response.json()) as Listicle;
 
@@ -18,33 +22,35 @@ export function routeData({ params }: RouteDataArgs) {
       return data;
     },
     {
-      key: () => params.id,
+      key: params.id,
     }
   );
 }
 
 export default function List() {
   const initialListicle = useRouteData<typeof routeData>();
-  // const [newListicle, setNewListicle] = createSignal<Listicle>();
+  const [newListicle, setNewListicle] = createSignal<Listicle>();
 
-  // const latestListicle = () => newListicle() ?? initialListicle();
+  const latestListicle = () => newListicle() ?? initialListicle.latest;
 
   return (
     <>
-      <A
-        href="/listicles"
-        class="py-2 px-4 bg-purple-600 text-white rounded-md disabled:bg-gray-400"
-      >
-        Back to all Listicles
-      </A>
-
       <div class="block my-4">
+        <div class="block my-8">
+          <A
+            href="/listicles"
+            class="py-2 px-4 bg-purple-600 text-white rounded-md disabled:bg-gray-400"
+          >
+            Back to all Listicles
+          </A>
+        </div>
+
         <Suspense fallback={<Loader />}>
-          <Title>{initialListicle()?.title}</Title>
+          <Title>{latestListicle()?.title}</Title>
           <h3 class="text-2xl text-orange-700 my-4">
-            {initialListicle()?.title}
+            {latestListicle()?.title}
           </h3>
-          <For each={initialListicle()?.entries ?? []}>
+          <For each={latestListicle()?.entries ?? []}>
             {(item) => <p class="font-bold">{item.text}</p>}
           </For>
         </Suspense>
